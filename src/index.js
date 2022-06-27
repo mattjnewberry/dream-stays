674d84080f8b53ff90cf53baa70a0a425905e11b
Matt Newberry <manewberry@expediagroup.com>
2022-06-27 18:46:39 +0100

    Update static files

--- src/index.js
+++ src/index.js
@@ -2,15 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, HashRouter } from "react-router-dom";
 import Stay from "./routes/stays";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter>
+  <HashRouter>
     <Routes>
       <Route path="/" element={<App />} />
       <Route path="/:stayId" element={<Stay />} />
     </Routes>
-  </BrowserRouter>
+  </HashRouter>
 );
