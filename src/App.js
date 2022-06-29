65f3498d5b8da7faf653833c7a1f6d69d4f30d38
Matt Newberry <manewberry@expediagroup.com>
2022-06-29 16:20:59 +0100

    Add h2 to test ci

--- src/App.js
+++ src/App.js
@@ -1,27 +1,29 @@
 import "./App.css";
 import React from "react";
 import Card from "./components/Card";
-import {getStays} from "./data";
+import { getStays } from "./data";
 
 export default function App() {
-    let stays = getStays();
-    return (
-        <div className="app">
-            <header className="app-header">
-                <h1>Dream Stays</h1>
-            </header>
-            <div className="container">
-                {stays.map((stay) => {
-
-                    return <Card
-                        key={stay.id}
-                        id={stay.id}
-                        name={stay.name}
-                        description={stay.description}
-                        img={stay.img}
-                    />
-                })}
-            </div>
-        </div>
-    );
+  let stays = getStays();
+  return (
+    <div className="app">
+      <header className="app-header">
+        <h1>Dream Stays</h1>
+      </header>
+      <h2>Test CI Workflow</h2>
+      <div className="container">
+        {stays.map((stay) => {
+          return (
+            <Card
+              key={stay.id}
+              id={stay.id}
+              name={stay.name}
+              description={stay.description}
+              img={stay.img}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
 }
