b4c0a910688006ad2bb5429591cc9c6b4b4f2dc2
Khadiza <91006311+khadizaa@users.noreply.github.com>
2022-06-30 14:44:30 +0100

    england

--- src/App.js
+++ src/App.js
@@ -9,8 +9,8 @@ export default function App() {
     <div className="app">
       <header className="app-header">
         <h1>Dream Stays</h1>
-      </header>
-      <h2>London</h2>
+      <header className="sub-heading">
+      <h2>England</h2>
       <div className="container">
         {stays.map((stay) => {
           return (
