93fda5a89be45b62860a7fabe876ccdbb2531709
Selin <138572754+SelinC0@users.noreply.github.com>
2023-07-05 15:23:08 +0000

    Update 2 files

--- src/App.js
+++ src/App.js
@@ -2,32 +2,32 @@ import "./App.css";
 import React from "react";
 import Card from "./components/Card";
 import { getStays } from "./data";
-import useState from "react";
-
+import { useState } from "react";
 
 // Main app
 export default function App() {
-  const egBlue = "#3662d8"; 
+  const egBlue = "#3662d8";
   const [bgColor, setBgColor] = useState(egBlue);
-  
+
   const changeColor = () => {
     let purple = "#A020F0";
     setBgColor(purple);
-   };
-<button type="button" onClick={changeColor}>
-    Click me!
-</button>
+  };
+
   let stays = getStays();
   return (
     <div className="app">
-      <header className="app-header">
-        style={{ background: bgColor }}
+      <header className="app-header" style={{ background: bgColor }}>
         <p className="disclaimer">
           This is a fictional website for educational purposes
         </p>
         <h1>Dream Stays: Norway </h1>
         <p className="app-slogan">Let The Aurora Amaze You!</p>
-      </header> Have A Dream Life
+        <button type="button" onClick={changeColor}>
+          Click me!
+        </button>
+      </header>
+      <p>Have A Dream Life</p>
       <br />
       <div className="container">
         {stays.map((stay) => {
@@ -45,20 +45,19 @@ export default function App() {
       <hr className="app-divider" />
       <div>
         <h2 className="app-subheader">Climate</h2>
-        <p>
-          Norway has a maritime climate with mild winters and cool summers.
-        </p>
+        <p>Norway has a maritime climate with mild winters and cool summers.</p>
       </div>
       <hr className="app-divider" />
       <div>
         <h2 className="app-subheader">History</h2>
         <p>
-          The history of Norway has been influenced to an extraordinary degree by the terrain and the climate of the region. About 10,000 BC, following the retreat inland of the great ice sheets, the earliest inhabitants migrated north into the territory which is now Norway. 
+          The history of Norway has been influenced to an extraordinary degree
+          by the terrain and the climate of the region. About 10,000 BC,
+          following the retreat inland of the great ice sheets, the earliest
+          inhabitants migrated north into the territory which is now Norway.
         </p>
-      </div>  
-      <div className="footer">2023
-    </div>
       </div>
-      
+      <div className="footer">2023</div>
+    </div>
   );
 }
