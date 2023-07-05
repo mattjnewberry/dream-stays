5bad46187c2aabaed8f15c8c6e6a2c2818221bde
Selin <138572754+SelinC0@users.noreply.github.com>
2023-07-05 15:12:59 +0000

    Update App.css and App.js

--- src/App.js
+++ src/App.js
@@ -2,18 +2,31 @@ import "./App.css";
 import React from "react";
 import Card from "./components/Card";
 import { getStays } from "./data";
+import useState from "react";
+
 
 // Main app
 export default function App() {
+  const egBlue = "#3662d8"; 
+  const [bgColor, setBgColor] = useState(egBlue);
+  
+  const changeColor = () => {
+    let purple = "#A020F0";
+    setBgColor(purple);
+   };
+<button type="button" onClick={changeColor}>
+    Click me!
+</button>
   let stays = getStays();
   return (
     <div className="app">
       <header className="app-header">
+        style={{ background: bgColor }}
         <p className="disclaimer">
           This is a fictional website for educational purposes
         </p>
         <h1>Dream Stays: Norway </h1>
-        <p className="app-slogan">Let The Aurora Amaze You!</p>!
+        <p className="app-slogan">Let The Aurora Amaze You!</p>
       </header> Have A Dream Life
       <br />
       <div className="container">
@@ -43,7 +56,7 @@ export default function App() {
           The history of Norway has been influenced to an extraordinary degree by the terrain and the climate of the region. About 10,000 BC, following the retreat inland of the great ice sheets, the earliest inhabitants migrated north into the territory which is now Norway. 
         </p>
       </div>  
-      <div className="footer">
+      <div className="footer">2023
     </div>
       </div>
       
